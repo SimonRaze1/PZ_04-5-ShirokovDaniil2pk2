6ff0fff88acf0bcc64591cdc76de8757b4c3e578
User <User@DESKTOP-AMGP8UK>
2023-09-23 10:32:59 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PZ_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0, j = 30; i + j != 8; i++, j--)
+            {
+                Console.WriteLine("i:" + i + ", j:" + j);
+            }
+        }
+    }
+}
